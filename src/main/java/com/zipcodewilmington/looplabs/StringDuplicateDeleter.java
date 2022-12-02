75a78e063496c3d901ed1b8aed8755e5afaf4794
Collin <collin@Zipcoders-MBP.hsd1.de.comcast.net>
2022-12-01 21:15:27 -0500

    same methodology works for Strings as well, all complete and tests passing

--- src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
+++ src/main/java/com/zipcodewilmington/looplabs/StringDuplicateDeleter.java
@@ -1,5 +1,7 @@
 package com.zipcodewilmington.looplabs;
 
+import java.util.Arrays;
+
 /**
  * Created by leon on 1/28/18.
  * @ATTENTION_TO_STUDENTS You are forbidden from modifying the signature of this class.
@@ -11,11 +13,33 @@ public final class StringDuplicateDeleter extends DuplicateDeleter<String> {
 
     @Override
     public String[] removeDuplicates(int maxNumberOfDuplications) {
-        return new String[0];
+        String[] newArr = new String[array.length];
+        int index = 0;
+
+        for (String elem : array){
+            if (NumOfOccurences(elem) < maxNumberOfDuplications){
+                newArr[index] = elem;
+                index++;
+            }
+        }
+        return Arrays.copyOf(newArr, index);
+
+        //return new String[0];
     }
 
     @Override
     public String[] removeDuplicatesExactly(int exactNumberOfDuplications) {
-        return new String[0];
+        String[] newArr = new String[array.length];
+        int index = 0;
+
+        for (String elem : array){
+            if (NumOfOccurences(elem) != exactNumberOfDuplications){
+                newArr[index] = elem;
+                index++;
+            }
+        }
+        return Arrays.copyOf(newArr, index);
+
+        //return new String[0];
     }
 }
